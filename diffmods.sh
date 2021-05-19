3f7690bffddade67fafa9d4b579ad3fa4c4af492
rene-d <rene-d@users.noreply.github.com>
2021-05-19 04:59:41 +0200

    compare two archive

---
+++ diffmods.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ $# != 2 ]]; then
+    echo "Usage: $0 <file1.sh> <file2.sh>"
+    echo
+    echo "Compare two downloaded module list"
+else
+    diff <("$1" -t | sort -u) <("$2" -t | sort -u) | sed -nr '/@/s/(^[^@]+@v[^/]+).*$/\1/p' | uniq
+fi
