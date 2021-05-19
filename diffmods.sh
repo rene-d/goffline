101490c3c073dd6b5248ae984944c7b312ac41a5
rene-d <rene-d@users.noreply.github.com>
2021-05-19 05:32:59 +0200

    do not show sumdb

--- diffmods.sh
+++ diffmods.sh
@@ -5,5 +5,5 @@ if [[ $# != 2 ]]; then
     echo
     echo "Compare two downloaded module list"
 else
-    diff <("$1" -t | sort -u) <("$2" -t | sort -u) | sed -nr '/@/s/(^[^@]+@v[^/]+).*$/\1/p' | uniq
+    diff <("$1" -t | sort -u) <("$2" -t | sort -u) | sed -nr '/@/s/(^[^@]+@v[^/]+).*$/\1/p' | grep -v "sumdb/sum\.golang\.org" | uniq
 fi
